e322b43502b29cb8d3206b46486e804a24323252
jartigag <jartigag@protonmail.com>
2018-05-11 18:39:37 +0200

    claps working. permanent counters needed

--- js/clap.js
+++ js/clap.js
@@ -1,17 +1,13 @@
 var clap = document.getElementsByClassName('clap');
 var clapCount = document.getElementsByClassName('clap--count');
 var numberOfClaps = [];
-for (var i = 0; i < clap.length; i++) numberOfClaps[i] = 0;
+for (var i = 0; i<clap.length; i++) numberOfClaps[i] = 0;
 
-for (var i = 0; i < clap.length; i++) {
-  clap[i].addEventListener('click', function () {
-    updateNumberOfClaps();
-  });
-}
+for (var i = 0; i<clap.length; i++)	clap[i].addEventListener('click', updateNumberOfClaps(i));
 
-function updateNumberOfClaps() {
-  for (var i = 0; i < clap.length; i++) {
-    numberOfClaps[i] < 50 ? numberOfClaps[i]++ : null;
-    clapCount[i].innerHTML = "+" + numberOfClaps[i];
-  }
+function updateNumberOfClaps(i) {
+	return function() {
+		numberOfClaps[i] < 50 ? numberOfClaps[i]++ : null;
+		clapCount[i].innerHTML = "+" + numberOfClaps[i];
+	}
 }
